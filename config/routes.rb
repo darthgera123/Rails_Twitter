928c67bc2c75fe6e3493c0fc1fe29346c8f28b3f
Pulkit gera <pulkithifi@yahoo.com>
2020-01-20 02:37:03 +0530

    Testing added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  # this will set the home page and get are the RESTAPI methods root '<controller name>#action'
+  root 'static_pages#home'
 end
